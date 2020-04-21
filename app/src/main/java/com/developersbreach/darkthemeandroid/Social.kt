eb9de7d1a8256be07cddff62bc8450c4d09a6245
rajas <rajashekarraju123@gmail.com>
2020-04-21 23:53:15 +0530

    Converted java classes to kotlin

--- app/src/main/java/com/developersbreach/darkthemeandroid/Social.kt
+++
@@ -1,7 +0,0 @@
-package com.developersbreach.darkthemeandroid
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Social(val icon: Int, val title: String, val subtitle: String) : Parcelable
